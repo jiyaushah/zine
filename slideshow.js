d424c672150004f6998cbce071ab91518721cdce
jiyaushah <jiyautpalshah@gmail.com>
2025-04-28 04:19:25 -0400

    zine

---
+++ slideshow.js
@@ -0,0 +1,169 @@
+const slidesData = [
+  {
+    imageUrl: 'images/page1.png',
+    imageSources: " https://www.india.com/webstories/news/india/10-amazing-and-interesting-facts-about-shri-kedarnath-temple-6041250/"
+  },
+  {
+    imageUrl: 'images/page2.png',
+    description: `The story of Kedarnath’s Pilgrimages begins with Lord Shiva, the destroyer. He is the God of Destruction, Time, Yoga, Meditation, and the Arts <a href='https://www.bbc.co.uk/religion/religions/hinduism/deities/shiva.shtml
+'>[1]</a>, among other things, one of the major deities of the Hindu pantheon of divine beings and one of the 3 Gods that cyclically create, maintain, and destroy the universe <a href='https://www.thebuddhagarden.com/shiva-hindu-god-destruction.html
+' target='_blank'>[2]</a>. In the story of Kedarnath, it is believed that he took the form of a bull and burrowed himself into the Earth, after having been angered by the Pandavas committing the sin of fratricide during the events of the <i>Mahabharata</i> <a href='https://www.namasteindiatrip.com/kedarnath-history.html'>[3]</a>.`,
+    imageSources: "https://isha.sadhguru.org/mahashivratri/shiva/names-of-shiva-with-meaning/ and \nhttps://www.metmuseum.org/art/collection/search/77650 "
+  },
+  {
+    imageUrl: 'images/page3.png',
+    description: `The Pandavas, who were a group of five powerful brothers, hoped to seek forgiveness from Lord Shiva. After following the advice of the sage, Vyas, they set out to look for Shiva to seek forgiveness. Bhima, the strongest of the brothers, saw Lord Shiva as he sank into the Earth. In an attempt to halt his descent and ask for forgiveness, he began pulling at Lord Shiva’s tail, which pleased Shiva enough to grant forgiveness <a href='https://www.namasteindiatrip.com/kedarnath-history.html'>[3]</a>.`,
+    imageSources: "https://www.namasteindiatrip.com/kedarnath-history.html and \nRavi Varma Press B"
+  },
+  {
+    imageUrl: 'images/page4.png',
+    imageSources: "https://timesofindia.indiatimes.com/travel/destinations/kedarnath-temple-in-uttarakhand-5-interesting-facts-to-know/articleshow/113713503.cms and \nhttps://www.namasteindiatrip.com/kedarnath-temple.html",
+    description: "Several parts of him came out of the Earth, specifically, his hands, face, hump, and legs. The hump is contained by the Kedarnath temple, which was made in gratitude of Lord Shiva by the Pandavas <a href='https://www.namasteindiatrip.com/kedarnath-history.html'>[3]</a>. This, in addition to the other three temples that contain the parts of his body, is part of the <i>Chota Char Dham Yatra</i> (lit. ‘Small Four Seat Pilgrimage’)<a href='https://web.archive.org/web/20140502222038/http://www.hindustantimes.com/india-news/dehradun/char-dham-yatra-kicks-off-as-portals-open/article1-1214974.aspx'>[4]</a>. In order to seek forgiveness, gain spiritual enlightenment, and thereby receive <i>moksha</i> (lit. ‘freedom from <i>samsara</i>, the cycle of life, death, and rebirth’), this is one of the things that Hindus should complete."
+  },
+  {
+    imageUrl: 'images/page5.png',
+    description: "The path up to Kedarnath involves a 16-kilometer trek in the Himalayas from the nearest town, Gaurikund, at a gain of 5,072 feet in altitude to an elevation of ~11,755 feet <a href='https://himalayandreamtreks.in/kedarnath-trek-yatra/#:~:text=Difficulty%20Level%20of%20Kedarnath,risk%20of%20AMS%20and%20Fatigue'>[5]</a>. For pilgrims making the journey, it represents a number of things: first, it is a proof of devotion and a step towards achieving <i>moksha</i>, as it brings human beings closer to a spot of divinity. More specifically, Kedarnath is a <i>Jyotirlinga</i> (lit. ‘mark of light’), which is a representation of Lord Shiva’s divinity <a href='https://www.wisdomlib.org/definition/jyotirlinga' target='_blank'>[6]</a>.",
+    imageSources: "my mother, Devangi Shah"
+  },
+  {
+    imageUrl: 'images/page6.png',
+    description: 'Furthermore, it is surrounded by the beauty of nature and thereby represents an ideal spot for meditation, where one can pray to Shiva and fulfill their desires <a href="https://servdharm.com/blogs/hindu/why-devotees-go-to-kedarnath?srsltid=AfmBOoqKCAqsmqPIOSHJu7c0M2Q6A9W6_oYSwOgvhAv4LAQofOjqgQ_T" target="_blank">[7]</a>. In Hinduism, nature and divinity are inherently intertwined, as nature, and thereby everything in it, are manifestations of the divine, and therefore, it must be respected and worshipped <a href=”https://www.bgci.org/files/Worldwide/Education/Seeds_of_Unity/Presentations/hinduism_plants_and_environment.pdf “ target= “_blank”>[8]</a>, similar to womanist beliefs of the sacredness of all things. Thus, meditating in the valley of Kedarnath allows for a connection to the sacred, both through thought/prayer and contact with divinity in its multiple forms.',
+    imageSources: "my mother, Devangi Shah"
+  },
+  {
+    imageUrl: 'images/page7.png',
+    description: 'Lastly, Kedarnath is also the site of the last sighting of Adi Shankaracharya, the Hindu philosopher credited with revitalizing Hinduism in the 8th century. This statue is representative of his Samadhi (lit. ‘deep meditative concentration’), a state that leads to self-realization <a href=”https://www.inidesignstudio.com/portfolio/shri-adi-guru-shankaracharya-samadhi/” target= “_blank”>[9]</a>. It is both meant to honor him and represent the self-actualization that the journey to Kedarnath and the other temples of the Chota Char Dham Yatra brings.',
+    imageSources: "my mother, Devangi Shah"
+  },
+  {
+    description: 'As a Hindu, one of my goals in life is to complete the pilgrimage to Kedarnath, along with the other pilgrimages that progress the attainment of moksha, furthering my path towards self-realization. Furthermore, because my mother has gone on this pilgrimage, undertaking it is a way for me to follow in her footsteps. Even the idea of being able to partake in something that not only many members of my family but also generations of Hindus before me have done brings me a sense of awe, as it truly reinforces the idea of a divine interconnection for me. Furthermore, Kedarnath embodies the notion of physical connections to the spiritual that we have discussed throughout the course; it is literally a space of spiritual and holy connection where the physical journey that one undertakes on the trek to Kedarnath is parallel to the spiritual and metaphorical course their life takes.',
+    imageSources: "my mother, Devangi Shah"
+  },
+  {
+    title: 'Works Cited',
+    description: `
+  [1] BBC. “BBC - Religions - Hinduism: Shiva.” Bbc.co.uk, 24 Aug. 2009, <a href="https://www.bbc.co.uk/religion/religions/hinduism/deities/shiva.shtml" target="_blank">www.bbc.co.uk/religion/religions/hinduism/deities/shiva.shtml</a>.<br>
+  [2] “Shiva: Hindu God of Destruction - the Buddha Garden.” Www.thebuddhagarden.com, <a href="https://www.thebuddhagarden.com/shiva-hindu-god-destruction.html" target="_blank">www.thebuddhagarden.com/shiva-hindu-god-destruction.html</a>.<br>
+  [3] Dass, Parmeshwar. “History of Kedarnath - the Origin Story of Kedarnath Dham.” Namaste India Trip, 3 Mar. 2019, <a href="https://www.namasteindiatrip.com/kedarnath-history.html" target="_blank">www.namasteindiatrip.com/kedarnath-history.html</a>.<br>
+  [4] Times, Hindustan. “Char Dham Yatra Kicks off as Portals Open - Hindustan Times.” Hindustan Times, 2025, <a href="https://web.archive.org/web/20140502222038/www.hindustantimes.com/india-news/dehradun/char-dham-yatra-kicks-off-as-portals-open/article1-1214974.aspx" target="_blank">web.archive.org/web/20140502222038/www.hindustantimes.com/india-news/dehradun/char-dham-yatra-kicks-off-as-portals-open/article1-1214974.aspx</a>.<br>
+  [5] “Kedarnath Yatra 2025 | Complete Guide, Cost, Itinerary.” Himalayan Dream Treks, 25 Apr. 2025, <a href="https://himalayandreamtreks.in/kedarnath-trek-yatra/" target="_blank">himalayandreamtreks.in/kedarnath-trek-yatra/</a>.<br>
+  [6] Wisdomlib.org. “Jyotirlinga, Jyotirlimga, Jyotirliṅga, Jyotis-Linga: 5 Definitions.” Wisdomlib.org, 28 Dec. 2019, <a href="https://www.wisdomlib.org/definition/jyotirlinga" target="_blank">www.wisdomlib.org/definition/jyotirlinga</a>.<br>
+  [7] Serv Dharm. “Why Devotees Go to Kedarnath.” ServDharm, 18 Aug. 2022, <a href="https://servdharm.com/blogs/hindu/why-devotees-go-to-kedarnath?srsltid=AfmBOoqKCAqsmqPIOSHJu7c0M2Q6A9W6_oYSwOgvhAv4LAQofOjqgQ_T" target="_blank">servdharm.com/blogs/hindu/why-devotees-go-to-kedarnath</a>.<br>
+  [8] Pattni, Ramesh. Hinduism, Plants and Environment, <a href="https://www.bgci.org/files/Worldwide/Education/Seeds_of_Unity/Presentations/hinduism_plants_and_environment.pdf" target="_blank">www.bgci.org/files/Worldwide/Education/Seeds_of_Unity/Presentations/hinduism_plants_and_environment.pdf</a>.<br>
+  [9] “Shri Adi Guru Shankaracharya Samadhi - INI Design Studio.” INI Design Studio, 4 Apr. 2025, <a href="https://www.inidesignstudio.com/portfolio/shri-adi-guru-shankaracharya-samadhi/" target="_blank">www.inidesignstudio.com/portfolio/shri-adi-guru-shankaracharya-samadhi/</a>.
+    `,
+  type: 'worksCited'
+  },
+  {
+    title: 'Image Citations (In Order)',
+    description: `
+  "Names of Shiva with Meanings." Isha Foundation, <a href="https://isha.sadhguru.org/mahashivratri/shiva/names-of-shiva-with-meaning/" target="_blank">isha.sadhguru.org/mahashivratri/shiva/names-of-shiva-with-meaning/</a>.<br>
+  "Shiva as Lord of Dance (Nataraja)." The Metropolitan Museum of Art, <a href="https://www.metmuseum.org/art/collection/search/77650" target="_blank">metmuseum.org/art/collection/search/77650</a>.<br>
+  "History of Kedarnath Temple." Namaste India Trip, <a href="https://www.namasteindiatrip.com/kedarnath-history.html" target="_blank">namasteindiatrip.com/kedarnath-history.html</a>.<br>
+  "Kedarnath Temple Interesting Facts." Times of India, <a href="https://timesofindia.indiatimes.com/travel/destinations/kedarnath-temple-in-uttarakhand-5-interesting-facts-to-know/articleshow/113713503.cms" target="_blank">timesofindia.indiatimes.com/travel/destinations/kedarnath-temple-in-uttarakhand-5-interesting-facts-to-know/articleshow/113713503.cms</a>.<br>
+  "Kedarnath Temple." Namaste India Trip, <a href="https://www.namasteindiatrip.com/kedarnath-temple.html" target="_blank">namasteindiatrip.com/kedarnath-temple.html</a>.<br>
+  "Print of Bhima by Ravi Varma Press." (Historical print, Ravi Varma Press, c. 19th–early 20th century.)<br>
+  "Photos of Kedarnath Pilgrimage, taken by Devangi Shah." Personal photograph, 2024.
+  `,
+    imageSources: "Compiled by Jiya Shah",
+  type: 'imageCitations'
+  }
+  
+];
+
+
+const slideshowContainer = document.getElementById('slideshow');
+const tooltip = document.getElementById('tooltip');
+let currentSlide = 0;
+
+slidesData.forEach((item) => {
+  const slide = document.createElement('div');
+  slide.className = 'slide';
+
+  if (item.imageUrl) {
+    const img = document.createElement('img');
+    img.src = item.imageUrl;
+    img.alt = 'Slide Image';
+
+    img.addEventListener('click', (e) => {
+      if (item.imageSources) {
+        showTooltip(e.clientX, e.clientY, item.imageSources);
+      }
+    });
+
+    slide.appendChild(img);
+  }
+
+  if (item.description) {
+    const textDiv = document.createElement('div');
+    textDiv.className = 'description';
+  
+    if (item.type === 'worksCited' || item.type === 'imageCitations') {
+      textDiv.classList.add('small-text');
+    }
+  
+    // ✨ Create and add a title if it exists
+    if (item.title) {
+      const titleElement = document.createElement('h2');
+      titleElement.className = 'slide-title';
+      titleElement.textContent = item.title;
+      textDiv.appendChild(titleElement);
+    }
+  
+    const textContent = document.createElement('div');
+    textContent.className = 'description-content';
+    textContent.innerHTML = item.description;
+  
+    textDiv.appendChild(textContent);
+    slide.appendChild(textDiv);
+  }
+  
+
+  if (!item.imageUrl) {
+    slide.classList.add('text-only');
+  }
+  
+
+  slideshowContainer.appendChild(slide);
+});
+
+
+const slides = document.querySelectorAll('.slide');
+
+showSlide(currentSlide);
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('show');
+  });
+  slides[index].classList.add('show');
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  hideTooltip();
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  hideTooltip();
+  showSlide(currentSlide);
+}
+
+function showTooltip(x, y, text) {
+  tooltip.textContent = text;
+  tooltip.style.left = `${x + 10}px`; 
+  tooltip.style.top = `${y + 10}px`;
+  tooltip.style.opacity = 1;
+}
+
+function hideTooltip() {
+  tooltip.style.opacity = 0;
+}
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('img')) {
+    hideTooltip();
+  }
+});
